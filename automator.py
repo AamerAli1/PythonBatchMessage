25a3c79ab24fe1373eb60c9426c2224788a6fff6
Amer <aamermurhaf@gmail.com>
2021-01-19 21:15:44 +0300

    Formatting

--- automator.py
+++ automator.py
@@ -1,54 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException, UnexpectedAlertPresentException, NoAlertPresentException
+from selenium.common.exceptions import TimeoutException, \
+    UnexpectedAlertPresentException, NoAlertPresentException
 from time import sleep
 from urllib.parse import quote
 from sys import platform
 import pandas
 
 options = Options()
-driver = webdriver.Chrome("drivers\\chromedriver.exe", options=options)
+driver = webdriver.Chrome('drivers\\chromedriver.exe', options=options)
 driver.get('https://web.whatsapp.com')
 count = 0
 delay = 30
 
 excel_data = pandas.read_excel('test2.xlsx', sheet_name='Sheet1')
-input("Press ENTER after login into Whatsapp Web and your chats are visiable.")
+input('Press ENTER after login into Whatsapp Web and your chats are visiable.'
+      )
 for column in excel_data['Full Name'].tolist():
+
     # takes message from excel
+
     message = excel_data['Arb Message'][0]
-    message = message.replace('{بيب}', str(excel_data['BIB Number'][count]))
-    message= message.replace('arabic_run_distance}', str(excel_data['{Arabic Run Distance}'][count]))
-    #message = message.replace('{bib_ number}', str(excel_data['BIB Number'][count]))
-    #message = message.replace('{run_distance}', str(excel_data['Run Distance'][count]))
+    message = message.replace('{بيب}',
+                              str(excel_data['BIB Number'][count]))
+    message = message.replace('arabic_run_distance}',
+                              str(excel_data['{Arabic Run Distance}'
+                              ][count]))
 
+    # message = message.replace('{bib_ number}', str(excel_data['BIB Number'][count]))
+    # message = message.replace('{run_distance}', str(excel_data['Run Distance'][count]))
 
+    # take numbers from excel
 
-
-
-
-
-
-
-    #take numbers from excel
     number = str(excel_data['Phone Number'][count])
     count = count + 1
 
     try:
-        url = 'https://web.whatsapp.com/send?phone=' + number + '&text=' + message
+        url = 'https://web.whatsapp.com/send?phone=' + number \
+            + '&text=' + message
         driver.get(url)
         try:
-            click_btn = WebDriverWait(driver, delay).until(EC.element_to_be_clickable((By.CLASS_NAME , '_2Ujuu')))
-        except (UnexpectedAlertPresentException, NoAlertPresentException) as e:
-            print("alert present")
+            click_btn = WebDriverWait(driver,
+                    delay).until(EC.element_to_be_clickable((By.CLASS_NAME,
+                                 '_2Ujuu')))
+        except (UnexpectedAlertPresentException,
+                NoAlertPresentException), e:
+            print 'alert present'
             Alert(driver).accept()
         sleep(1)
         click_btn.click()
         sleep(3)
-        print('Message sent to: ' + number)
-    except Exception as e:
-        print('Failed to send message to ' + number + str(e))
+        print 'Message sent to: ' + number
+    except Exception, e:
+        print 'Failed to send message to ' + number + str(e)
